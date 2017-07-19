0bf62a253852868206fd7d4ba2e286b09837fde6
黑牛哞哞 <763209836@qq.com>
2017-07-19 16:02:43 +0800

    集成retrifit和okhht功能

--- app/src/main/java/sj/com/voiceclock/App.java
+++ app/src/main/java/sj/com/voiceclock/App.java
@@ -3,6 +3,8 @@ package sj.com.voiceclock;
 import android.app.Application;
 import android.content.Context;
 
+import com.iflytek.cloud.SpeechUtility;
+
 /**
  * Created by Administrator on 2017/7/18 0018.
  */
@@ -14,6 +16,7 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         app = this;
+        SpeechUtility.createUtility(this,"appid=" + getString(R.string.app_id));
     }
 
     public static Context getContext(){
